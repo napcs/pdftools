ead9986b31f6a1697b28d3d0b3e106a3459eb0e8
Brian Hogan <brianhogan@napcs.com>
2011-04-03 17:36:11 -0500

    added output switch

--- lib/pdftools.rb
+++ lib/pdftools.rb
@@ -1,4 +1,5 @@
 require 'optparse'
+require 'fileutils'
 require 'pdftools/tools'
 require 'pdftools/version'
 JAR_ROOT = File.expand_path("../../jar/", __FILE__)
--- lib/pdftools/tools.rb
+++ lib/pdftools/tools.rb
@@ -1,9 +1,15 @@
 module PDFTools
   class Tools
 
-    def self.merge(files)
+    def self.merge(options)
+      files = options[:files]
+      outfile = options[:outfile]
       cmd = "java -Xss1024K -Xmx512m -cp #{JAR_ROOT}/multivalent*.jar tool.pdf.Merge -samedoc #{files.join(" ")}"
       system(cmd)
+      
+      if outfile
+        FileUtils.mv files.first.gsub(".pdf", "-m.pdf"), outfile
+      end
     end
     
   end
